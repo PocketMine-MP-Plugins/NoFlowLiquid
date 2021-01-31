ea34a1c82d1a051e22af6ae693c24618d4860423
GoGoAirPlane <42131095+neoskr@users.noreply.github.com>
2021-01-31 22:53:29 +0900

    Create NoFlowLiquid.php

---
+++ NoFlowLiquid.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @name NoFlowLiquid
+ * @main NoFlowLiquid\NoFlowLiquid
+ * @author SYNK
+ * @version x
+ * @api x
+ * @description x
+ */
+
+
+namespace NoFlowLiquid;
+
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+use pocketmine\Server;
+use pocketmine\block\Liquid;
+use pocketmine\event\block\BlockSpreadEvent;
+
+
+class NoFlowLiquid extends PluginBase implements Listener
+{
+
+	public function onEnable () : void
+	{
+
+		Server::getInstance()->getPluginManager()->registerEvents ($this, $this);
+
+	}
+
+	public function onSpread(BlockSpreadEvent $event) : void
+	{
+		if (!$event->getSource() instanceof Liquid)	
+			return;
+		$event->setCancelled(true);
+	}
+
+}
